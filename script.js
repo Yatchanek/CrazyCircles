c1192a28db54b22e8c3f90afb23fdbf71f5405f2
yatchanek <yatsu@gazeta.pl>
2020-09-26 20:05:34 +0200

    Some new features & bug fixes

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ let lastFrame = 0
 let lastEntitySpawn = 0
 let pointerX, pointerY
 let screenMin = Math.min(screenHeight, screenWidth)
-let score = 0
+let score = highScore = 0
 let level = 1
 let fails = 0
+let missedCircles = 0
 let circleCount = 0
 let spawnFrequency = 2000
 let frameCount = 0
@@ -57,21 +58,23 @@ class ScoreBubble {
 
 
 class Circle {
-    constructor(x, y) {
+    constructor(x, y, type = 'circle') {
         this.x = x
         this.y = y
         this.radius = random(screenMin * 0.15, screenMin * 0.2)
-        this.firstRadius = this.radius
+        this.initialRadius = this.radius
         this.weight = random(screenMin * 0.01, screenMin * 0.025)
         
         this.direction = Math.random() < 0.5 ? 1 : - 1
         this.rotationSpeed = random(3, 12)
         this.vx = this.vy = 0
         this.shrinkSpeed = Math.min(this.radius, random(this.radius * 0.3 + 0.05 * level, this.radius * 0.4 + 0.05 * level))
+        
         if (level > 4 && Math.random() < 0.1 + 0.05 * (level - 5)) {
             this.vx = random(screenMin * 0.15, screenMin * 0.25) * (Math.random() < 0.5 ? this.direction : -this.direction)
             this.vy = random(screenMin * 0.15, screenMin * 0.25) * (Math.random() < 0.5 ? this.direction : -this.direction)
         }
+        
         this.dead = false
 
         this.pieces = random(5,15)
@@ -79,7 +82,10 @@ class Circle {
         for (let i = 0; i < this.pieces; i++) {
             this.colors.push(`rgb(${random(100, 255)}, ${random(10, 255)}, ${random(10, 255)})`)
         }
-        this.type = 'circle'
+        this.type = type
+        if (this.type === 'bonus') {
+            this.shrinkSpeed *= 2
+        }
     }
 
     draw() {
@@ -91,6 +97,13 @@ class Circle {
             ctx.arc(this.x, this.y, this.radius, i * TAU / this.pieces + this.direction * frameCount / this.rotationSpeed, (i + 1) * TAU / this.pieces + + this.direction * frameCount / this.rotationSpeed,)
             ctx.stroke()
         }
+        if (this.type === 'bonus') {
+            ctx.beginPath()
+            ctx.arc(this.x, this.y, this.radius, 0, TAU)
+            ctx.fillStyle = `rgba(${200 + 55 * Math.sin(frameCount / 10)}, 0, 0, ${0.7 + 0.3 * Math.sin(frameCount / 10)})`
+            ctx.fill()
+        }
+
         ctx.restore()
     }
 
@@ -99,6 +112,12 @@ class Circle {
         if (this.radius < 0) {
             this.radius = 0
             this.dead = true
+            if (this.type === 'circle') {
+                missedCircles++
+                score -= 50
+                let sb = new ScoreBubble(this.x, this.y, -50, this.colors, this.initialRadius * 0.8)
+                scoreBubbles.push(sb)
+            }
         }
         this.x += this.vx * delta
         this.y += this.vy * delta
@@ -117,9 +136,9 @@ class Circle {
 class Polygon extends Circle {
     constructor(x, y) {
         super(x, y)
-        this.weight = random(screenMin * 0.005, screenMin * 0.01)
+        this.weight = random(screenMin * 0.005, screenMin * 0.015)
         this.rotationSpeed = random(7,15)
-        this.pieces = random(4,8)
+        this.pieces = random(4, 6 + Math.floor(level / 2))
         this.colors = []
         for (let i = 0; i < this.pieces; i++) {
             this.colors.push(`rgb(${random(100, 255)}, ${random(10, 255)}, ${random(10, 255)})`)
@@ -171,13 +190,15 @@ function checkHit(e) {
         nextState = 'COUNTDOWN'
         spawnEntity(false)
         lastEntitySpawn = performance.now()
+
     }
+
     else {
         for (entity of entities) {
             if((e.pageX - entity.x) * (e.pageX - entity.x) + (e.pageY - entity.y) * (e.pageY - entity.y) < 
                 entity.radius * entity.radius) {
                 entity.dead = true
-                let points = Math.ceil(50 * entity.radius / entity.firstRadius)
+                let points = Math.ceil(50 * entity.radius / entity.initialRadius)
                 if (entity.type === 'circle') {
                     score += points
                     if (++circleCount % (10 * level) === 0) {
@@ -188,44 +209,57 @@ function checkHit(e) {
                             spawnFrequency = 250
                         }
                     }
+                } else if (entity.type === 'bonus') {
+                    points *= 1.5
+                    let which = Math.random()
+                    if (which < 0.5) {
+                        fails > 0 ? fails-- : missedCircles--
+                    } else {
+                        missedCircles > 0 ? missedCircles-- : fails--
+                    }
+
                 } else {
                     points *= -3
                     score += points
                     fails++
                 }
     
-                if (fails > 3) {
-                    nextState = 'GAMEOVER'
-                }
-    
                 let sb = new ScoreBubble(entity.x, entity.y, points, entity.colors, entity.radius)
                 scoreBubbles.push(sb)
+                break
             }
         }
     }   
+
 }
 
 function spawnEntity(allowPolygon) {
     let buff = screenMin * 0.2
-    let x = random(buff, screenWidth - buff)
-    let y = random(buff, screenHeight - buff)
     let entity, type
     if (allowPolygon) {
         type = Math.random() > 0.04 + 0.015 * level ? 'circle' : 'polygon'
     }
     else type = 'circle'
     
+    if ((missedCircles > 0 || fails > 0) && Math.random() < 0.02) {
+        type = 'bonus'
+    } 
+
     let canPlace = true
     do {
-        type === 'circle' ? entity = new Circle(x, y) : entity = new Polygon(x, y)
-        for (let i = 0; i < entities.length;i++) {
-            if ((this.x - entities[i].x) * (this.x - entities[i].x) + (this.y - entities[i].y) * (this.y - entities[i].y) <
-            (this.radius + entities[i].radius) * (this.radius + entities[i].radius)) {
+        let x = random(buff, screenWidth - buff)
+        let y = random(buff, screenHeight - buff)
+        type === 'circle' || type === 'bonus' ? entity = new Circle(x, y, type) : entity = new Polygon(x, y)
+        for (let i = 0; i < entities.length; i++) {
+            if ((entity.x - entities[i].x) * (entity.x - entities[i].x) + (entity.y - entities[i].y) * (entity.y - entities[i].y) <
+            (entity.radius + entities[i].radius) * (entity.radius + entities[i].radius)) {
                 canPlace = false
                 break
             }
+            canPlace = true
         }
-    } while(!canPlace)
+        
+    } while(canPlace === false)
     
     entities.push(entity)
 }
@@ -239,8 +273,13 @@ function gameLoop(timestamp) {
     if (gameState === 'GAMEOVER') {
         entities = []
         scoreBubbles = []
+        if (score > highScore) {
+            highScore = score
+            localStorage.setItem('CrazyCirclesHighScore', highScore)
+        }
         score = 0
         fails = 0
+        missedCircles = 0
         countdown = 3
 
         ctx.fillStyle = 'rgb(255, 255, 255)'
@@ -275,16 +314,19 @@ function gameLoop(timestamp) {
             nextState = 'PLAY'
             spawnEntity(false)
             lastEntitySpawn = timestamp
-        }
+            }
     }
     if (gameState === 'PLAY') {
         ctx.fillStyle = 'rgb(255, 255, 255)'
         ctx.font = `${screenMin * 0.04}px 'Bernard MT Condensed', Impact`
-        ctx.fillText(`Score: ${score}`, 20, screenMin * 0.03)
-        ctx.fillStyle = `rgb(255, ${255 - 80 * fails}, ${255 - 80 * fails})`
-        ctx.fillText(`Fails: ${fails}`, 20, screenMin * 0.085)
+        ctx.fillText(`High Score: ${highScore}`, 20, screenMin * 0.03)
+        ctx.fillText(`Score: ${score}`, 20, screenMin * 0.085)
+        ctx.fillStyle = `rgb(255, ${255 - 85 * fails}, ${255 - 85 * fails})`
+        ctx.fillText(`Fails: ${fails}`, 20, screenMin * 0.145)
+        ctx.fillStyle = `rgb(255, ${255 - 25 * missedCircles}, ${255 - 25 * missedCircles})`
+        ctx.fillText(`Missed: ${missedCircles}`, 20, screenMin * 0.2)
         ctx.fillStyle = 'rgb(255, 255, 255)'
-        ctx.fillText(`Level: ${level}`, 20, screenMin * 0.145)
+        ctx.fillText(`Level: ${level}`, 20, screenMin * 0.255)
     
         entities = entities.filter(entity => entity.dead === false)
         entities.forEach(c => {
@@ -297,7 +339,10 @@ function gameLoop(timestamp) {
             sb.update(delta)
             sb.draw()
         })
-    
+        
+        if (fails > 3 || missedCircles > 10) {
+            nextState = 'GAMEOVER'
+        }
     
         if (timestamp - lastEntitySpawn > spawnFrequency && entities.length < 7) {      
             spawnEntity(true)
@@ -309,6 +354,10 @@ function gameLoop(timestamp) {
 }
 
 //Main Program :)
+if (localStorage.hasOwnProperty('CrazyCirclesHighScore')) {
+    highScore = localStorage.getItem('CrazyCirclesHighScore')
+}
+
 window.requestAnimationFrame(gameLoop)
 
 window.addEventListener('pointerdown', e => checkHit(e))
@@ -317,3 +366,6 @@ window.addEventListener('resize', () => {
     gameWindow.height = screenHeight = window.innerHeight
     screenMin = Math.min(screenHeight, screenWidth)
 })
+// audioElement.addEventListener('ended', () => {
+//     audioElement.play()
+// })
